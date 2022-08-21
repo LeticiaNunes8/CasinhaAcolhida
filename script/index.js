8f6aa6b33c360d77ed627ce21e1aa78b21a791d6
Leticia <leticianunesq8@gmail.com>
2022-08-21 16:53:14 -0300

    scroll da pagina na aba de contatos

---
+++ script/index.js
@@ -0,0 +1,16 @@
+const contato = document.querySelector("#contatoScroll")
+contato.addEventListener("click", () => {
+    window.scroll({top: window.innerHeight} , 10000)
+})
+
+
+
+
+
+
+
+/*$("#anchor").click(function() {
+    $('html, body').animate({
+      scrollBottom: $("#anchor").offset().bottom
+    }, 2000);
+  });*/
